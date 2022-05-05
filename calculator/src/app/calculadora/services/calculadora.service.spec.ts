1c57c22ae880bbd6f12469a47e7a45dde7ba30f8
Felipe Trosi <felipetrosi@outlook.com>
2022-05-05 16:47:01 -0300

    Intial Commit, basic calculator project.

---
+++ calculator/src/app/calculadora/services/calculadora.service.spec.ts
@@ -0,0 +1,40 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CalculadoraService } from './calculadora.service';
+
+describe('CalculadoraService', () => {
+  let service: CalculadoraService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [CalculadoraService]      
+    });
+    service = TestBed.inject(CalculadoraService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('deve garantir que 3 + 2 = 5', () => {
+    let btn3 = fixture.debugElement.query(By.css('#btn3'));
+    let btnSoma = fixture.debugElement.query(By.css('#btnSoma'));
+    let btn2 = fixture.debugElement.query(By.css('#btn2'));
+    let btnCalcular = fixture.debugElement.query(By.css('#btnCalcular'));
+    let display = fixture.debugElement.query(By.css('#display'));
+
+    btn3.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnSoma.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    
+    btnCalcular.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('5');
+  }); 
+});
